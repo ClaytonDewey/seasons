4cb9ad2524ee7f4193d155cb3673fb4c317249bc
ClaytonDewey <clay@dryadmedia.com>
2022-06-02 09:07:23 -0500

    Refactor to class component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SeasonDisplay } from "./SeasonDisplay";
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position.coords.latitude),
-        (err) => console.error(err)
-    );
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position.coords.latitude),
+            (err) => console.error(err)
+        );
 
-    return <h1>Hi there!</h1>;
-};
+        return <h1>Latitude: </h1>;
+    }
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
