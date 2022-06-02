1f1b21e20c3d0f7a5942356372c7c32ddd426e07
ClaytonDewey <clay@dryadmedia.com>
2022-06-02 08:36:14 -0500

    Setup geolocation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom/client";
 import { SeasonDisplay } from "./SeasonDisplay";
 
 const App = () => {
-    return <SeasonDisplay />;
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position.coords.latitude),
+        (err) => console.error(err)
+    );
+
+    return <h1>Hi there!</h1>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
