bfecf1f6424b0c8617be54e88e6e6320f9dd4c89
ClaytonDewey <clay@dryadmedia.com>
2022-06-06 10:14:48 -0500

    Show Icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,10 +11,13 @@ const getSeason = (lat, month) => {
 export const SeasonDisplay = ({ lat }) => {
     const season = getSeason(lat, new Date().getMonth());
     const text = season === "winter" ? "Brrr, it is chilly" : "Let's hit the beach";
+    const icon = season === "winter" ? "snowflake" : "sun";
 
     return (
         <div>
+            <i className={`icon ${icon}`} />
             <h1>{text}</h1>
+            <i className={`icon ${icon}`} />
         </div>
     );
 };
